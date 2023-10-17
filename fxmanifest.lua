227d4c79016f735fcae93dfcde74729615211d3f
Sav <tekpro145@gmail.com>
2023-10-17 10:30:44 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+game 'gta5'
+
+lua54 'yes'
+
+author 'Sav - discord.gg/dev'
+description 'Optimized weapon holster script'
+version '1.0.0'
+
+client_scripts {
+    'cl_holsterweapon.lua'
+}
